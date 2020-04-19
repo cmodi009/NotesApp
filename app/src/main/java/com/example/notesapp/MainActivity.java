5070a8fc4d435e8cd6ca64b3efe8675addad1332
Chintan Modi <chintan.modi@somaiya.edu>
2020-04-19 17:19:10 +0530

    added notedetails

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.notesapp.model.Adapter;
 import com.google.android.material.navigation.NavigationView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     NavigationView nav_view;
-
+    RecyclerView noteLists;
+    com.example.notesapp.model.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +34,56 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        noteLists = findViewById(R.id.notelist);
 
         drawerLayout  = findViewById(R.id.drawer);
         nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
+
         toggle= new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
+
+        List<String> titles = new ArrayList<>();
+        List<String> content = new ArrayList<>();
+
+        titles.add("First Note Title.");
+        content.add("First Note Content sample.");
+
+        titles.add("Second Note Title.");
+        content.add("second Note Content sample.second Note Content sample.second Note Content sample.second Note Content sample.");
+
+        titles.add("Third Note Title.");
+        content.add("Third Note Content sample.");
+
+        adapter = new Adapter(titles,content);
+        noteLists.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        noteLists.setAdapter(adapter);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            default:
+                Toast.makeText(this, "Coming Soon...", Toast.LENGTH_SHORT).show();
+        }
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.settings)
+        {
+            Toast.makeText(this, "Settings menu is clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/notesapp/model/Adapter.java
@@ -0,0 +1,98 @@
+package com.example.notesapp.model;
+
+import android.content.Intent;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.notesapp.NoteDetails;
+import com.example.notesapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    List<String> titles;
+    List<String> content;
+
+    public Adapter(List<String> title,List<String> content){
+        this.titles = title;
+        this.content = content;
+    }
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_view_layout,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, final int position) {
+        holder.noteTitle.setText(titles.get(position));
+        holder.noteContent.setText(content.get(position));
+        final int code = getRandomColor();
+        holder.mCardView.setCardBackgroundColor(holder.view.getResources().getColor(code,null));
+
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Item is clicked", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(v.getContext(), NoteDetails.class);
+                i.putExtra("title",titles.get(position));
+                i.putExtra("content",content.get(position));
+                i.putExtra("code",code);
+                v.getContext().startActivity(i);
+
+            }
+        });
+    }
+
+    private int getRandomColor()
+    {
+        List<Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.blue);
+        colorCode.add(R.color.yellow);
+        colorCode.add(R.color.skyblue);
+        colorCode.add(R.color.lightPurple);
+        colorCode.add(R.color.lightGreen);
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.red);
+        colorCode.add(R.color.greenlight);
+        colorCode.add(R.color.notgreen);
+
+        Random randomColor = new Random();
+        int number = randomColor.nextInt(colorCode.size());
+        return colorCode.get(number);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView noteTitle,noteContent;
+        View view;
+        CardView mCardView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noteTitle = itemView.findViewById(R.id.titles);
+            noteContent = itemView.findViewById(R.id.content);
+            mCardView = itemView.findViewById(R.id.noteCard);
+            view = itemView;
+        }
+    }
+}
